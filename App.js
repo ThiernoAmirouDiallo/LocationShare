6ebd52d4bb5c3eeba1cf0340a36c5c0be66ab28e
Thierno Amirou Diallo <diallo110339@MacBook-Pro-de-Amirou.local>
2018-06-02 20:30:28 +0000

    added redux

--- App.js
+++ App.js
@@ -1,66 +1,47 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
-import placeImage from './src/assets/beautiful-place.jpeg'
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail'
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+import {
+  addPlace,
+  deletePlace,
+  selectPlace,
+  deselectPlace
+} from "./src/store/actions/index";
 
-export default class App extends Component {
-  state = {
-    places: [],
-      selectedPlace: null,
-  };
-
+class App extends Component {
   placeAddedHandler = placeName => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-            key: Math.random(),
-            name:placeName,
-            image: {
-                uri: "https://www.wallpapersforiphone6plus.com/wp-content/uploads/2018/01/Beach-Wallpaper-Desktop-Free-75-with-Beach-Wallpaper-Desktop-Free.jpg"
-            }
-        })
-      };
-    });
+    this.props.onAddPlace(placeName);
   };
 
-  placeDeletedHandler = () =>{
-      this.setState(prevState => {
-        return {
-          places : prevState.places.filter((place)=>
-          {
-            return place.key !== prevState.selectedPlace.key;
-          }),
-            selectedPlace:null
-        };
-      });
-  }
+  placeDeletedHandler = () => {
+    this.props.onDeletePlace();
+  };
 
   modalClosedHandler = () => {
-      this.setState(
-          {selectedPlace: null}
-          )
-  }
+    this.props.onDeselectPlace();
+  };
 
   placeSelectedHandler = key => {
-      this.setState(prevState => {
-          return {
-              selectedPlace : prevState.places.find(place => {
-                  return place.key === key;
-              })
-          };
-      })
-
+    this.props.onSelectPlace(key);
   };
 
   render() {
     return (
       <View style={styles.container}>
-          <PlaceDetail selectedPlace={this.state.selectedPlace} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHandler}/>
+        <PlaceDetail
+          selectedPlace={this.props.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler} />
+        <PlaceList
+          places={this.props.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
       </View>
     );
   }
@@ -75,3 +56,21 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start"
   }
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: name => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: key => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
