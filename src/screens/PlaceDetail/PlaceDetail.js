8431c22d9774f034800596e2be0c4b98da261f2d
Thierno Amirou DIALLO <thiernoamiroud@gmail.com>
2018-07-07 22:36:48 +0000

    pop a page from the stack

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import { View, Image, Text, Button, StyleSheet,TouchableOpacity } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons'
-const placeDetail = props => {
+import Icon from 'react-native-vector-icons/Ionicons';
 
-  return (
+import { connect } from 'react-redux';
+import {deletePlace} from '../../store/actions/index';
 
-      <View style={styles.container}>
-          <View>
-              <Image source={props.selectedPlace.image} style={styles.placeImage} />
-              <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-          </View>
-        <View>
-          <TouchableOpacity onPress={props.onItemDeleted}>
-            <View style={styles.deleteButton}>
-              <Icon size={30} name="ios-trash" color="red"/>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
 
-  );
-};
+class PlaceDetail extends Component {
+  placeDeletedHandler = () => {
+    this.props.onDeletePlace(this.props.selectedPlace.key);
+    this.props.navigator.pop();
+  }
+   render() {
+     return (
+         <View style={styles.container}>
+             <View>
+                 <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+                 <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+             </View>
+             <View>
+                 <TouchableOpacity onPress={this.placeDeletedHandler}>
+                     <View style={styles.deleteButton}>
+                         <Icon size={30} name="ios-trash" color="red"/>
+                     </View>
+                 </TouchableOpacity>
+             </View>
+         </View>
+     );
+}
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -40,4 +48,12 @@ const styles = StyleSheet.create({
     }
 });
 
-export default placeDetail;
+const mapDispatchToProps = dispatch=> {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+
+    };
+};
+
+export default connect(null,mapDispatchToProps)(PlaceDetail);
+
