4b6c32e6c6fcdf8d344e7d8dcca329ebb14c6646
Thierno Amirou DIALLO <thiernoamiroud@gmail.com>
2018-07-07 23:28:17 +0000

    added a side Dawer

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawerScreen from './src/screens/SideDrawer/SideDrawer';
 
 import {Provider } from 'react-redux';
 
@@ -16,6 +17,7 @@ Navigation.registerComponent("awesome-places.AuthScreen",()=>AuthScreen, store, 
 Navigation.registerComponent("awesome-places.SharePlaceScreen",()=>SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.FindPlaceScreen",()=>FindPlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.PlaceDetailScreen",()=>PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SideDrawerScreen",()=>SideDrawerScreen);
 
 //Start App
 Navigation.startSingleScreenApp({
---
+++ src/screens/SideDrawer/SideDrawer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {View,Text,Dimensions,StyleSheet} from 'react-native';
+
+class SideDrawer extends Component{
+    render() {
+        return (
+            <View style={{width: Dimensions.get("Window").width*0.8}}>
+                <Text>
+                    SideDrawer
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default SideDrawer;
