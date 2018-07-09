a36da4c344b0fe5331cf6613a103efdbd227a7d7
Thierno Amirou DIALLO <thiernoamiroud@gmail.com>
2018-07-09 18:57:26 +0000

    using Platform API

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -2,17 +2,12 @@ import React, { Component } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 
 import DefaultInput from '../UI/DefaultInput/DefaultInput'
-class PlaceInput extends Component {
-
-
-  render() {
-    return (
-          <DefaultInput placeholder="Place Name"
-          value={this.props.placeName}
-          onChangeText={this.props.onChangeText }/>
+const placeInput =props => (
+          <DefaultInput
+              placeholder="Place Name"
+              value={props.placeName}
+              onChangeText={props.onChangeText }
+          />
     );
-  }
-}
 
-
-export default PlaceInput;
+export default placeInput;
