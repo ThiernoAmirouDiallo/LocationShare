c4415da92654b66b3047d4f49a8b376e1df23e09
Thierno Amirou DIALLO <thiernoamiroud@gmail.com>
2018-07-11 21:53:32 +0000

    using validation state to validate a form

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -22,21 +22,24 @@ class AuthScreen extends Component{
                 valid:false,
                 validationRules:{
                     isEmail:true
-                }
+                },
+                touched: false
             },
             password:{
                 value: "",
                 valid:false,
                 validationRules:{
                     minLength:6
-                }
+                },
+                touched: false
             },
             confirmPassword:{
                 value: "",
                 valid:false,
                 validationRules:{
                     equalsTo:"password"
-                }
+                },
+                touched: false
             }
         }
     }
@@ -96,7 +99,8 @@ class AuthScreen extends Component{
                     [key] : {
                         ...prevState.controls[key],
                         value:value,
-                        valid: validate(value,prevState.controls[key].validationRules,connectedValue)
+                        valid: validate(value,prevState.controls[key].validationRules,connectedValue),
+                        touched:true
                     }
                 }
             };
@@ -125,6 +129,8 @@ class AuthScreen extends Component{
                                       onChangeText={(val)=> {
                                           this.updateInputState("email", val)
                                       }}
+                                      valid={this.state.controls.email.valid}
+                                      touched={this.state.controls.email.touched}
                         />
                         <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
                             <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
@@ -132,18 +138,31 @@ class AuthScreen extends Component{
                                               value={this.state.controls.password.value}
                                               onChangeText={(val)=> {
                                                   this.updateInputState("password", val)
-                                              }}/>
+                                              }}
+                                              valid={this.state.controls.password.valid }
+                                              touched={this.state.controls.password.touched }
+                                />
                             </View>
                             <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
                                 <DefaultInput placeholder={"Confirm Password"} style={styles.input}
                                               value={this.state.controls.confirmPassword.value}
                                               onChangeText={(val)=> {
                                                   this.updateInputState("confirmPassword", val)
-                                              }}/>
+                                              }}
+                                              valid={this.state.controls.confirmPassword.valid }
+                                              touched={this.state.controls.confirmPassword.touched }
+                                />
                             </View>
                         </View>
                     </View>
-                    <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler }>Submit</ButtonWithBackground>
+                    <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler }
+                    disabled={
+                        !this.state.controls.email.valid ||
+                        !this.state.controls.password.valid ||
+                        !this.state.controls.confirmPassword.valid
+                    }>
+                        Submit
+                    </ButtonWithBackground>
                 </View>
             </ImageBackground>
 
